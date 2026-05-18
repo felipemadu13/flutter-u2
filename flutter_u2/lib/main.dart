f058320285a57725f0c081e04c4f3ce476ac042c
felipemadu13 <felipemadureira.a@hotmail.com>
2026-05-18 19:43:51 -0300

    feat: Mudança no design da página de login

--- flutter_u2/lib/main.dart
+++ flutter_u2/lib/main.dart
@@ -12,33 +12,68 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
       ),
       home: Scaffold(
       body: Center(
-        child: Column(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget> [
-            Text('Minha aplicação'),
+            Text('Minha aplicação', style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+            SizedBox(height: 30.0),
+            
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'usuário',
+                label: Row(
+                  children: [
+                    Icon(Icons.person, color: Colors.grey),
+                    SizedBox(width: 5),
+                    Text('Usuário'),
+                  ],
+                ),
                 border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey),
               ),
             ),
+            SizedBox(height: 10.0),
             TextFormField(
               decoration: InputDecoration(
-                labelText: 'senha',
+                                label: Row(
+                  children: [
+                    Icon(Icons.lock, color: Colors.grey),
+                    SizedBox(width: 5),
+                    Text('Senha'),
+                  ],
+                ),
                 border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Colors.grey),
               ),
             ),
+            SizedBox(height: 10.0),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 167, 22, 53),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // borda arredondada
+                ),
+              ),
               onPressed: () {}, child: const Text('Login')
             ),
-            Text( 'Esqueceu a senha?'),
-            Text('Não tem uma conta? Cadastre-se'),
+            SizedBox(height: 20.0),
+            Text( 'Esqueceu a senha?', style: TextStyle(color: Colors.grey)),
+            SizedBox(height: 5.0),
+            Text('Não tem uma conta? Cadastre-se', style: TextStyle(color: Colors.grey)),
           ],
         ),
+
+
+        ),
+        
       ),
      )
     );
